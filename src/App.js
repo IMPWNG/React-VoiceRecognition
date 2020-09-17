f7919e93254e86f25e779e3a18695aa953c3bfff
IMPWONG <matisse.acheen.pro@gmail.com>
2020-09-17 16:43:38 -0400

    News on console log

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey = '418529240ff59a392ddc25d3e070a5fe2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 
 const App = () => {
-
     useEffect(() => {
         alanBtn({
-            key: alanKey,
-            onCommand: ({ command }) => {
-                if(command === 'testCommand') {
-                    alert('This code was executed');
+          key: '418529240ff59a392ddc25d3e070a5fe2e956eca572e1d8b807a3e2338fdd0dc/stage',
+          onCommand: ({ command, articles }) => {
+            if (command === 'newHeadlines') {
+              console.log(articles);
                 }
             }
         })
