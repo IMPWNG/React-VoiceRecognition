d8ab6e143db42c6fdace93bad0434e50bc532f05
IMPWONG <matisse.acheen.pro@gmail.com>
2020-09-18 11:13:30 -0400

    News call & Display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+
+
     useEffect(() => {
         alanBtn({
           key: '418529240ff59a392ddc25d3e070a5fe2e956eca572e1d8b807a3e2338fdd0dc/stage',
           onCommand: ({ command, articles }) => {
             if (command === 'newHeadlines') {
-              console.log(articles);
+                setNewsArticles(articles);
                 }
-            }
-        })
-    }, [])
-
+            },
+        });
+    }, []);
 
     return (
         <div>
-        <h1>Alain AI News App</h1>
+            <h1>Alain AI News App</h1>
+                <NewsCards articles={newsArticles} />
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
+
+import NewsCard from './NewsCard/NewsCard';
+import useStyles from './styles.js';
+
+const NewsCards = ({ articles }) => {
+    const classes = useStyles();
+
+
+    return (
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={6} md={5} lg={3} style={{ display: 'flex'}}>
+                    <NewsCard article={article} i={i} />
+                    </ Grid>
+                ))}
+            </Grid>
+        </Grow>
+    );
+};
+
+export default NewsCards;
