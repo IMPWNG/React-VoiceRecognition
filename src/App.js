6cf2a6472adeb1124b8775ea6a99db6e5de9038a
IMPWONG <matisse.acheen.pro@gmail.com>
2020-09-18 12:45:05 -0400

    Read the Headlines

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js'
 
 
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
 
 
@@ -23,7 +25,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alain AI News App</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+            </div>
                 <NewsCards articles={newsArticles} />
         </div>
     );
